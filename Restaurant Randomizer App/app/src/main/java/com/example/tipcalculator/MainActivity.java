40f749a6cd98b77dfd8d457855a8a2082d9be0ca
jakehiggott <JakeHiggott1@gmail.com>
2021-10-11 21:38:53 -0400

    Added basic UI One giant mess pull at your own risk. Needs constraints

---
+++ Restaurant Randomizer App/app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.tipcalculator;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+        /*init15Button();
+        init18Button();
+        init20Button();*/
+
+    }
+    /*private void init15Button() {
+        Button displayButton = findViewById(R.id.button15);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextEnterTotal = findViewById(R.id.enterMoneyText);
+                TextView tipAmountDisplay = findViewById(R.id.tipAmountText);
+                String totalText = editTextEnterTotal.getText().toString();
+
+                //Just a Quick note I was unsure how to change the value from a String to a Double/ Float to do calculations so I looked it up.
+                //I Found the code below from here: https://www.quora.com/How-would-I-get-an-int-value-from-an-edit-text-view-in-Android-studio
+                //It shows how to do it with integers but I just swapped integer for double and it works fine.
+                double totalNumber = Double.parseDouble(totalText);
+                double tipTotal = (totalNumber * 0.15);
+                double totalNumberActual = totalNumber + tipTotal;
+                //Rounded Doubles to 2 decimals to make it look better.
+                String Display = String.format("Tip: $%.2f , Total Bill: $%.2f", tipTotal,totalNumberActual);
+                tipAmountDisplay.setText(Display);
+            }
+        });
+    }
+    private void init18Button() {
+        Button displayButton = findViewById(R.id.button18);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextEnterTotal = findViewById(R.id.enterMoneyText);
+                TextView tipAmountDisplay = findViewById(R.id.tipAmountText);
+                String totalText = editTextEnterTotal.getText().toString();
+
+                //Just a Quick note I was unsure how to change the value from a String to a Double/ Float to do calculations so I looked it up.
+                //I Found the code below from here: https://www.quora.com/How-would-I-get-an-int-value-from-an-edit-text-view-in-Android-studio
+                //It shows how to do it with integers but I just swapped integer for double and it works fine.
+                double totalNumber = Double.parseDouble(totalText);
+                double tipTotal = (totalNumber * 0.18);
+                double totalNumberActual = totalNumber + tipTotal;
+                //Rounded Doubles to 2 decimals to make it look better.
+                String Display = String.format("Tip: $%.2f , Total Bill: $%.2f", tipTotal,totalNumberActual);
+                tipAmountDisplay.setText(Display);
+            }
+        });
+    }
+
+    private void init20Button() {
+        Button displayButton = findViewById(R.id.button20);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editTextEnterTotal = findViewById(R.id.enterMoneyText);
+                TextView tipAmountDisplay = findViewById(R.id.tipAmountText);
+                String totalText = editTextEnterTotal.getText().toString();
+
+                //Just a Quick note I was unsure how to change the value from a String to a Double/ Float to do calculations so I looked it up.
+                //I Found the code below from here: https://www.quora.com/How-would-I-get-an-int-value-from-an-edit-text-view-in-Android-studio
+                //It shows how to do it with integers but I just swapped integer for double and it works fine.
+                double totalNumber = Double.parseDouble(totalText);
+                double tipTotal = (totalNumber * 0.20);
+                double totalNumberActual = totalNumber + tipTotal;
+                //Rounded Doubles to 2 decimals to make it look better.
+                String Display = String.format("Tip: $%.2f , Total Bill: $%.2f", tipTotal,totalNumberActual);
+                tipAmountDisplay.setText(Display);
+            }
+        });
+    }
+
+     */
+}
