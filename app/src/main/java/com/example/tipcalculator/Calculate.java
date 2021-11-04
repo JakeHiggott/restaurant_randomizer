0e66fd6d41be1ce7901930e0cada23067ef8de15
reanamate <38570421+reanamate@users.noreply.github.com>
2021-11-04 15:32:24 -0400

    restructured MainActivity starting to add location services. There are unsolved errors that keep the program from running.

---
+++ app/src/main/java/com/example/tipcalculator/Calculate.java
@@ -0,0 +1,12 @@
+package com.example.tipcalculator;
+
+import static android.location.Criteria.NO_REQUIREMENT;
+
+import android.location.LocationManager;
+
+public class Calculate {
+  //initialize variables
+
+
+
+}
--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -8,43 +8,42 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //initialize variables
+    Button SearchdisplayButton = findViewById(R.id.goToFindRestuarant);
+    Button ListdisplayButton = findViewById(R.id.goToFindRestuarant2);
+    Button SettingsdisplayButton = findViewById(R.id.settingsButton);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
-        favorites();
-        findRestuarant();
-        settings();
+        SearchdisplayButton.setOnClickListener(findRestaurant());
+        ListdisplayButton.setOnClickListener(favorites());
+        SettingsdisplayButton.setOnClickListener(settings());
 
 
     }
-    private void findRestuarant() {
-        Button displayButton = findViewById(R.id.goToFindRestuarant);
-        displayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, findrestuarant.class));
-            }
-        });
+    public View.OnClickListener findRestaurant() {
+
+        startActivity(new Intent(MainActivity.this, findrestuarant.class));
+
+        return null;
     }
-    private void favorites() {
-        Button displayButton = findViewById(R.id.goToFindRestuarant2);
-        displayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, favorites.class));
-            }
-        });
+
+    public View.OnClickListener favorites() {
+
+        startActivity(new Intent(MainActivity.this, favorites.class));
+
+        return null;
     }
-    private void settings() {
-        Button displayButton = findViewById(R.id.settingsButton);
-        displayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, settings.class));
-            }
-        });
+
+    public View.OnClickListener settings() {
+
+        startActivity(new Intent(MainActivity.this, settings.class));
+
+        return null;
     }
+
     /*
     private void init18Button() {
         Button displayButton = findViewById(R.id.button18);
