0a36f9a688b7d801408bb06bde6c6f767c150f76
reanamate <38570421+reanamate@users.noreply.github.com>
2021-11-18 14:18:21 -0500

    Location Services are working better. Will update Location but not everytime search is ran.

--- app/src/main/java/com/example/tipcalculator/findrestuarant.java
+++ app/src/main/java/com/example/tipcalculator/findrestuarant.java
@@ -1,5 +1,7 @@
 package com.example.tipcalculator;
 
+import static com.google.android.gms.location.LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -11,6 +13,8 @@ import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationRequest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +23,10 @@ import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.CancellationTokenSource;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
@@ -29,8 +36,10 @@ import java.util.Locale;
 public class findrestuarant extends AppCompatActivity {
 
     Switch switch1;
-    TextView textView5,textView12,textView13;
+    TextView textView5,textView12,textView13,loadingTextView;
     FusedLocationProviderClient fusedLocationProviderClient;
+    Location MockLocation;
+    private Object CancellationToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,7 @@ public class findrestuarant extends AppCompatActivity {
         textView5 = findViewById(R.id.textView5);
         textView12 = findViewById(R.id.textView12);
         textView13 = findViewById(R.id.textView13);
+        loadingTextView = findViewById(R.id.loadingTextView);
         Button button = findViewById(R.id.frSearchButton);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
@@ -66,6 +76,11 @@ public class findrestuarant extends AppCompatActivity {
                     }
 
                 }else{
+                    /* the start of setting custom location
+                    fusedLocationProviderClient.setMockMode(true);
+                    Geocoder geocoder = new Geocoder(findrestuarant.this,Locale.getDefault());
+                    List<Address> addresses = geocoder.
+                    fusedLocationProviderClient.setMockLocation(MockLocation);*/
                     textView12.setText("Custom");
                     textView13.setText("Custom");
                 }
@@ -77,24 +92,49 @@ public class findrestuarant extends AppCompatActivity {
 
     @SuppressLint("MissingPermission")
     private void getLocation() {
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+        loadingTextView.setVisibility(TextView.VISIBLE);
+        fusedLocationProviderClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, null).addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
                 Location location = task.getResult();
-                if(location != null){
-
-                    try {
-                        Geocoder geocoder = new Geocoder(findrestuarant.this, Locale.getDefault());
-                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                        textView12.setText("TEMPORARY Latitude: " + addresses.get(0).getLatitude());
-                        textView13.setText("TEMPORARY Longitude: " + addresses.get(0).getLongitude());
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                if(TryLocation(location) == null){
+                    fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            Location location = task.getResult();
+                            TryLocation(location);
+                        }
+                    });
                 }
+                loadingTextView.setVisibility(TextView.INVISIBLE);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingTextView.setVisibility(TextView.INVISIBLE);
             }
         });
+
+    }
+
+    private Object TryLocation(Location location){
+
+        if(location != null){
+
+            try {
+                Geocoder geocoder = new Geocoder(findrestuarant.this, Locale.getDefault());
+                List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                textView12.setText("TEMPORARY Latitude: " + addresses.get(0).getLatitude());
+                textView13.setText("TEMPORARY Longitude: " + addresses.get(0).getLongitude());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            return null;
+        }
+
+    return 0;
     }
 
     private void ButtonCheck() {
